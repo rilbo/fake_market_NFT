3aa2aac19b9e98cac829f93f501850bbd6face3b
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2022-11-23 23:15:04 +0100

    All is finish

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ function load() {
     const loader = document.getElementById('loader');
     loader.classList.remove('hidden');
 }
+
 function unLoad() {
     const loader = document.getElementById('loader');
     loader.classList.add('hidden');
