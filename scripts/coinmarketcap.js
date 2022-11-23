ef05565eea71383e2b9bb459e8949942feea699e
Valentin Muller <valmuller14@gmail.com>
2022-11-23 18:21:54 +0100

    modif about best crypto currency

--- scripts/coinmarketcap.js
+++ scripts/coinmarketcap.js
@@ -20,22 +20,30 @@ fetch(url + qString)
             let li = document.createElement("li");
             let price = myJson.data[i].quote.USD.price.toFixed(2);
             if(i == 0){
-                li.classList.add("flex","flex-row","justify-between","items-center","py-2","px-4","hover:bg-gray-100");
+                li.classList.add("flex", "items-center", "justify-between", "max-w-2xl", "px-8", "py-4", "mx-auto", "border", "border-blue-500", "cursor-pointer", "rounded-xl");
                 li.innerHTML = `
-                      <div>
-                        <h3 class="text-4xl">${myJson.data[i].name}</h3>
-                        <span class="">${myJson.data[i].symbol}</span>
-                      </div>
-                      <p class="text-4xl">${price}$</p>    
+                    <div class="flex items-center">
+                        <div>
+                            <h2 class="text-lg font-medium text-gray-700 sm:text-2xl">${myJson.data[i].name}</h2>
+                            <div class=" text-xs text-blue-500">
+                            ${myJson.data[i].symbol}
+                            </div>
+                        </div>
+                    </div>
+                    <h2 class="text-2xl font-semibold text-blue-600 sm:text-4xl">${price}$</h2>  
                 `;
             }else{
-                li.classList.add("flex","flex-row","justify-between","items-center","py-2","px-4","hover:bg-gray-100");
-                li.innerHTML = `
-                      <div>
-                      <h4 class="text-2xl">${myJson.data[i].name}</h4>
-                      <span>${myJson.data[i].symbol}</span>
+                li.classList.add("flex", "items-center", "justify-between", "max-w-2xl", "px-8", "py-4", "mx-auto", "border",  "cursor-pointer", "rounded-xl", "dark:border-gray-700")
+                li.innerHTML = ` 
+                      <div class="flex items-center">
+                            <div>
+                                <h2 class="text-lg font-medium text-gray-700 sm:text-2xl ">${myJson.data[i].name}</h2>
+                                <div class="text-xs text-blue-500  ">
+                                    ${myJson.data[i].symbol}
+                                </div>
+                            </div>
                       </div>
-                      <p class="text-2xl">${price}$</p>    
+                      <h2 class="text-2xl font-semibold text-gray-500 sm:text-4xl dark:text-gray-500">${price}$</h2>   
                 `;
             }
             ul.appendChild(li);
